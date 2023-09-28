8207fe57653a998342b6af5d1ff1d412232713e2
Ernane Sousa <ernane.computacao@gmail.com>
2023-09-27 21:51:56 -0300

    Adicionando Atividade 01

--- atividades/Atividade01.cs
+++ atividades/Atividade01.cs
@@ -6,7 +6,20 @@ class Atividade01
     public static void Executar()
     {
 
+        int[] vetor = new int[5];
 
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Informe o {i + 1} número: ");
+            vetor[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("Números digitados:");
+        foreach (int numero in vetor)
+        {
+            Console.WriteLine(numero);
+        }
 
     }
 }
