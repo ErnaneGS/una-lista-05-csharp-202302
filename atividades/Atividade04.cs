e5c4719351371697479d66d9b24b47682a3f929a
Ernane Sousa <ernane.computacao@gmail.com>
2023-10-04 19:53:43 -0300

    Adicionando Atividade 04

--- atividades/Atividade04.cs
+++ atividades/Atividade04.cs
@@ -5,8 +5,26 @@ class Atividade04
 
     public static void Executar()
     {
+        char[] caracteres = new char[10];
+        int consoante = 0;
 
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Informe o {i + 1}º caractere: ");
+            caracteres[i] = Convert.ToChar(Console.ReadLine());
+        }
 
+        Console.Write("-- Consoantes:");
+        foreach (char c in caracteres)
+        {
+            if (char.IsLetter(c) && !"AEIOUaeiou".Contains(c))
+            {
+                Console.Write($"{c} / ");
+                consoante++;
+            }
+        }
+
+        Console.WriteLine($"-- Quantidade de consoantes: {consoante} \n");
 
     }
 }
