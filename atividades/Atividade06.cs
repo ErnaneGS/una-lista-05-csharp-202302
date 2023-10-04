10491f311c769da84e85612b3f4373dae63e3512
Ernane Sousa <ernane.computacao@gmail.com>
2023-10-04 20:21:01 -0300

    Adicionando Atividade 06

--- atividades/Atividade06.cs
+++ atividades/Atividade06.cs
@@ -6,7 +6,30 @@ class Atividade06
     public static void Executar()
     {
 
+        double[] medias = new double[10];
+        int contador = 0;
 
+        for (int i = 0; i < 10; i++)
+        {
+            double soma = 0;
+            for (int j = 0; j < 4; j++)
+            {
+                Console.WriteLine($"\nInforme a {j + 1}ª nota do aluno {i + 1}");
+                soma = soma + Convert.ToDouble(Console.ReadLine());
+            }
 
+            medias[i] = soma / 4;
+
+            Console.WriteLine($"\n---Média do aluno {i + 1}: {medias[i]}");
+
+            if (medias[i] >= 7.0)
+            {
+                contador++;
+
+            }
+        }
+
+        Console.WriteLine($"---Alunos com média maior ou igual a 7.0: {contador}");
     }
+
 }
